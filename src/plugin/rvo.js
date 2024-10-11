ae3ac667911102f47388dd755f45914ca47158de
Jashon254 <158827004+Jashon254@users.noreply.github.com>
2024-10-12 00:15:44 +0300

    Update rvo.js

--- src/plugin/rvo.js
+++ src/plugin/rvo.js
@@ -22,7 +22,7 @@ const rvo = async (m, sock) => {
     const type = Object.keys(msg)[0];
     
     const originalCaption = msg[type].caption || '';
-    const newCaption = `${originalCaption}\n\n> © Powered By Trex-Md`;
+    const newCaption = `${originalCaption}\n\n> © Powered By DrJay-Md`;
 
 
     // Download the media content
