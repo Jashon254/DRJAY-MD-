e50e87bb7e9080a2ccb35caa9f235adbd5a3e81b
Jashon254 <158827004+Jashon254@users.noreply.github.com>
2024-10-12 02:29:48 +0300

    Update gdrive.js

--- src/plugin/gdrive.js
+++ src/plugin/gdrive.js
@@ -20,7 +20,7 @@ const gdriveDownload = async (m, Matrix) => {
 
       if (gdriveInfo && gdriveInfo.status && gdriveInfo.data) {
         const mediaUrl = gdriveInfo.data;
-        const caption = `> © Trex-Md`;
+        const caption = `> © DrJay-Md`;
 
         // Inferring the file type based on the file extension
         const extension = mediaUrl.split('.').pop().toLowerCase();
